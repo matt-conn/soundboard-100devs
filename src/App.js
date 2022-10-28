35f63337179cbe011da9c8e750d6a961a8fc3e36
Matt Connolly <81105533+matt-conn@users.noreply.github.com>
2022-10-27 21:01:31 -0400

    working on button bg imgs, unsuccessfully

--- src/App.js
+++ src/App.js
@@ -4,64 +4,74 @@ import Sounds from './components/Sounds'
 import './App.css';
 
 function App() {
-	const [isPlaying, setIsPlaying] = useState(false);
+	// const [isPlaying, setIsPlaying] = useState(false);
 
 	const [sounds] = useState([
 		{
 			id: 1,
 			text: 'We go get1',
 			sfx: new Audio(planeSfx),
+			isPlaying: false,
+			cover: './images/default-cover.png',
 		},
 		{
 			id: 2,
 			text: 'We go get2',
 			sfx: './sounds/small-plan.mp3',
+			isPlaying: false,
 		},
 		{
 			id: 3,
 			text: 'We go get3',
 			sfx: './sounds/small-plan.mp3',
+			isPlaying: false,
 		},
 		{
 			id: 4,
 			text: 'We go get4',
-			sfx: './sounds/small-plan.mp3',
+			sfx: new Audio(planeSfx),
+			isPlaying: false,
 		},
 		{
 			id: 5,
 			text: 'We go get5',
 			sfx: './sounds/small-plan.mp3',
+			isPlaying: false,
 		},
 		{
 			id: 6,
 			text: 'We go get6',
 			sfx: './sounds/small-plan.mp3',
+			isPlaying: false,
 		},
 		{
 			id: 7,
 			text: 'We go get7',
 			sfx: './sounds/small-plan.mp3',
+			isPlaying: false,
 		},
 		{
 			id: 8,
 			text: 'We go get8',
 			sfx: './sounds/small-plan.mp3',
+			isPlaying: false,
 		},
 		{
 			id: 9,
 			text: 'We go get9',
 			sfx: './sounds/small-plan.mp3',
+			isPlaying: false,
 		}
 	])
 
-	const playSound = (sfx) => {
-		if (!isPlaying) {
-			sfx.play();
+	const playSound = (sound) => {
+		if (!sound.isPlaying) {
+			sound.sfx.play();
 		} else {
-			sfx.pause();
+			sound.sfx.pause();
 		}
 
-		setIsPlaying(!isPlaying)
+		sound.isPlaying = !sound.isPlaying;
 	}
 
 	return (
--- src/components/Sound.js
+++ src/components/Sound.js
@@ -1,9 +1,25 @@
 const Sound = ({ sound, text, onClick }) => {
 	return (
-		<button onClick={() => onClick(sound.sfx)} className='button'>
+		<button 
+			onClick={() => onClick(sound)}
+			className='button'
+			style={{ 
+				backgroundImage: `url(/default-cover.png)`,
+				backgroundPosition: 'center',
+				backgroundRepeat:'no-repeat',
+				backgroundSize:'contain',
+				color: 'white'
+			}}
+		>
 			{text}
 		</button>
 	);
 };
 
 export default Sound;
+
+
+// background: url('./images/default-cover.png');
+// background-position: center;
+// background-size: cover;
+// background-repeat: no-repeat;
