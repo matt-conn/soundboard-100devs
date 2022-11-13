f2179b5d0146259a6d6b2d792ef4102d13fcec2a
Matt Connolly <81105533+matt-conn@users.noreply.github.com>
2022-11-13 10:21:23 -0500

    update styles

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -13,7 +13,7 @@ const Sound = ({ sound, text, onClick }) => {
 				}}
 			>
 				<span className="sound-label relative text-center">
-					<span className="block absolute -inset-1 -skew-y-3 bg-pink-500 drop-shadow-xl" aria-hidden="true">{text}</span>
+					<span className="block absolute -inset-1 -skew-y-3 bg-pink-500 border-2 border-pink-300 drop-shadow-xl" aria-hidden="true">{text}</span>
 					<span>{text}</span>
 				</span>
 			</button>
